1520a978ccf59f627ed33d0120772b8f75482caf
costassarris <costassarris@gmail.com>
2015-03-03 15:10:34 +0000

    game logic complete

--- spec/FizzbuzzSpec.js
+++ spec/FizzbuzzSpec.js
@@ -1,4 +1,52 @@
 describe("Javabuzz", function() {
-  var Fizzbuzz;
+  var fizzbuzz;
 
-})
+  beforeEach(function() {
+    fizzbuzz = new Fizzbuzz();
+  });
+
+  it("should know when a number is divisible by 3", function() {
+    expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+  });
+
+  it("should know when a number is NOT divisible", function() {
+    expect(fizzbuzz.isDivisibleByThree(4)).toBe(false);
+  });
+
+  it("should know when a number is divisible by 5", function() {
+    expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+  });
+
+  it("should know when a number is NOT divisible by 5", function() {
+    expect(fizzbuzz.isDivisibleByFive(4)).toBe(false);
+  });
+
+  it("should know when a number is divisible by 15", function() {
+    expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
+  });
+
+  it("should know when a number is NOT divisible by 15", function() {
+    expect(fizzbuzz.isDivisibleByFifteen(16)).toBe(false);
+  });
+
+  describe("when playing, says", function() {
+
+    it('"Fizz" when a number is divisible by 3', function() {
+      expect(fizzbuzz.says(3)).toEqual("Fizz");
+    });
+
+    it('"Buzz" when a number is divisible by 5', function() {
+      expect(fizzbuzz.says(5)).toEqual("Buzz");
+    });
+
+    it('"FizzBuzz" when a number is divisible by 15', function() {
+      expect(fizzbuzz.says(15)).toEqual("FizzBuzz");
+    });
+
+    it("the number if it is not divisible by 3 or 5", function() {
+      expect(fizzbuzz.says(4)).toEqual(4);
+    });
+
+  });
+
+});
